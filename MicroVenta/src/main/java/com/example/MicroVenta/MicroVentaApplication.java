4a6d1952f554eab71fb3a3bafe9306bc0a91227f
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 13:31:34 -0400

    Add files via upload

---
+++ MicroVenta/src/main/java/com/example/MicroVenta/MicroVentaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroVenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroVentaApplication.class, args);
+	}
+
+}
---
+++ MicroVenta/src/test/java/com/example/MicroVenta/MicroVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroVenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
